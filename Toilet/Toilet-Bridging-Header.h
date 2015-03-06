ff89cfb98817e3ce453bb51c0aec2081c02320b0
Yukihiro Moriyama <m.y.like.music.10@gmail.com>
2015-03-06 21:53:55 +0900

    Google Mapsを表示

---
+++ Toilet/Toilet-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Toilet-Bridging-Header.h
+//  Toilet
+//
+//  Created by yukihiro moriyama on 2015/03/06.
+//  Copyright (c) 2015年 YukihiroMoriyama. All rights reserved.
+//
+
+#ifndef Toilet_Toilet_Bridging_Header_h
+#define Toilet_Toilet_Bridging_Header_h
+
+#import <GoogleMaps/GoogleMaps.h>
+
+#endif
--- Toilet/ViewController.swift
+++ Toilet/ViewController.swift
@@ -8,11 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, GMSMapViewDelegate {
 
+    var gmaps : GMSMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        /* 地図の表示 */
+        let lat: CLLocationDegrees = 37.508435
+        let lon: CLLocationDegrees = 139.930696
+        let zoom: Float = 15
+        let camera: GMSCameraPosition = GMSCameraPosition.cameraWithLatitude(lat,longitude: lon,zoom: zoom);
+        
+        gmaps = GMSMapView(frame: CGRectMake(0, 0, self.view.bounds.width, self.view.bounds.height))
+        gmaps.camera = camera
+        self.view.addSubview(gmaps)
+        
+        /* マーカーの作成 */
+        let marker: GMSMarker = GMSMarker ()
+        marker.position = camera.target
+        marker.snippet = "Hello World"
+        marker.appearAnimation = kGMSMarkerAnimationPop;
+        marker.map = gmaps;
+        
     }
 
     override func didReceiveMemoryWarning() {
