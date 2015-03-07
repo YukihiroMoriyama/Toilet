82aad723244fa6d6c9df69c3d6ac673659944581
Yukihiro Moriyama <m.y.like.music.10@gmail.com>
2015-03-07 17:20:11 +0900

    現在位置をGoogle Maps上に表示

--- Toilet/ViewController.swift
+++ Toilet/ViewController.swift
@@ -19,19 +19,12 @@ class ViewController: UIViewController, GMSMapViewDelegate, CLLocationManagerDel
         // Do any additional setup after loading the view, typically from a nib.
         
         /* 地図の表示 */
-        //        let lat: CLLocationDegrees = 37.508435
-        //        let lon: CLLocationDegrees = 139.930696
-        //        let zoom: Float = 15
-        //        let camera: GMSCameraPosition = GMSCameraPosition.cameraWithLatitude(lat,longitude: lon,zoom: zoom);
-        
         gmaps = GMSMapView(frame: CGRectMake(0, 0, self.view.bounds.width, self.view.bounds.height))
-        //        gmaps.camera = camera
         gmaps.myLocationEnabled = true
         gmaps.delegate = self
         self.view.addSubview(gmaps)
         
         /* 現在位置の取得 */
-        /* TODO: 現在位置情報の取得不可 */
         lm = CLLocationManager()
         lm.delegate = self
         lm.requestAlwaysAuthorization()
@@ -45,14 +38,23 @@ class ViewController: UIViewController, GMSMapViewDelegate, CLLocationManagerDel
         NSLog("success")
         
         /* 現在位置を地図上に表示 */
-        var coordinate: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude:newLocation.coordinate.latitude,longitude:newLocation.coordinate.longitude)
-        var now :GMSCameraPosition = GMSCameraPosition.cameraWithLatitude(coordinate.latitude,longitude:coordinate.longitude,zoom:17)
+        var coordinate: CLLocationCoordinate2D = CLLocationCoordinate2D(
+            latitude: newLocation.coordinate.latitude,
+            longitude: newLocation.coordinate.longitude
+        )
+        
+        var now :GMSCameraPosition = GMSCameraPosition.cameraWithLatitude(
+            coordinate.latitude,
+            longitude: coordinate.longitude,
+            zoom: 17
+        )
+        
         gmaps.camera = now
         
         /* マーカーの作成 */
         let marker: GMSMarker = GMSMarker ()
         marker.position = now.target
-        marker.snippet = "Hello World"
+        marker.snippet = "現在位置だよー"
         marker.appearAnimation = kGMSMarkerAnimationPop;
         marker.map = gmaps;
     }
